f000dda0b2b873596b09cd0cfcebe02e0eb7ae2a
chomper <you@example.com>
2024-08-03 14:30:31 +0800

    fix device tree path

--- llama.cpp/firmware/load.sh
+++ llama.cpp/firmware/load.sh
@@ -2,7 +2,6 @@ make
 sudo sh -c "cp sgemm.bit /lib/firmware"
 sudo sh -c "echo sgemm.bit > /sys/class/fpga_manager/fpga0/firmware"
 
-sudo sh -c "rmdir /configfs/device-tree/overlays/k26-starter-kits_image_1"
 sudo sh -c "rmdir /configfs/device-tree/overlays/sgemm"
 sudo sh -c "mkdir /configfs/device-tree/overlays/sgemm"
 sudo sh -c "cat sgemm.dtbo > /configfs/device-tree/overlays/sgemm/dtbo"
